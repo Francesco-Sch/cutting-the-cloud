f5ec94554e87d06862e3999e3ccd29b21c34f1bb
Francesco Scheffczyk <francesco.scheffczyk@gmail.com>
2023-10-03 22:37:27 +0200

    Create web server that sends random numbers

---
+++ th-api/web_server.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+import random
+import time
+import threading
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def number_generator():
+    # This function simulates changes in the value you're reading from another device
+    prev_number = None
+    while True:
+        number = random.randint(1, 100)
+        if number != prev_number:  # checks if the number has changed
+            print(f"Number changed: {number}")
+            socketio.emit('number_update', {'number': number})
+            prev_number = number
+        time.sleep(5)
+
+if __name__ == "__main__":
+    t = threading.Thread(target=number_generator)
+    t.start()
+    socketio.run(app, debug=True)
